accae314df9ff78f878b4ec1f0a61db281a5649b
ShwetaBodake <shwetabodake8108@gmail.com>
2022-12-30 17:03:19 +0530

    Binary tree calling in main method

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+using BinaryTreeSearch;
+
+namespace BinaryTreeSearch
+{
+}
+public class Program
+{
+public static void Main(string[] args)
+    {
+        BinaryTree binaryTree = new BinaryTree();
+        binaryTree.Add(20);
+        binaryTree.Add(90);
+        binaryTree.Add(40);
+        binaryTree.Add(20);
+        binaryTree.Add(40);
+        binaryTree.Add(24);
+
+        binaryTree.TransversPreOrder(binaryTree.Root);
+
+    }
+}
